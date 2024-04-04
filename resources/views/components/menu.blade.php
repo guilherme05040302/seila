c215babc89a9de97dc2bb78bde73f84d7504d560
guilherme05040302 <guilhermea.calesso@gmail.com>
2024-04-04 10:54:52 -0300

    mercadolivre

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,71 @@
+<div class="navbar bg-yellow-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl text-black">Takaro Paul Nakara</a>
+  </div>
+  <div class="flex-none gap-2">
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto bg-white" />
+    </div>
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="navbar bg-yellow-300">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+       
+        <li>
+          <a>parents</a>
+          <ul class="p-2">
+            <li><a>Veículos</a></li>
+            <li><a>Mercado</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a class="btn btn-ghost text-xl text-black">Sujiro Kimimame</a>
+  </div>
+  <div class="navbar-center hidden text-black lg:flex">
+    <ul class="menu menu-horizontal px-1">
+     
+      <li>
+        <details>
+          <summary>Memataro Kotiro Nakara</summary>
+          <ul class="p-2">
+            <li><a>FUJIROKOA KOMBI</a></li>
+            <li><a>Taka Akara Numuru</a></li>
+            <li><a>Mekagotodo</a></li>
+            <li><a>Matamo Okasha</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Ofertas</a></li>
+      <li><a>ofertas</a></li>
+      <li><a>ofertas</a></li>
+      <li><a>ofertas</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    
+  </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<x-layout_base>
+
+<x-menu>
+
+</x-menu>
+<div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_797422-MLA75400551648_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_945326-MLA75548553029_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_667836-MLA74349586053_022024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_859602-MLA75368444934_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+
+</x-layout_base>
